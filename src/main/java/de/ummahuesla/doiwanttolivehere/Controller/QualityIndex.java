509d9b4e0762ccad4a71106aa46c01f978e34a60
bohnejoe <bohne-joe@arcor.de>
2018-05-04 16:43:43 +0200

    repository skeletons

--- src/main/java/de/ummahuesla/doiwanttolivehere/Controller/QualityIndex.java
+++ src/main/java/de/ummahuesla/doiwanttolivehere/Controller/QualityIndex.java
@@ -2,10 +2,16 @@ package de.ummahuesla.doiwanttolivehere.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QualityIndex {
+
+    @RequestMapping("/")
+    String index(@RequestParam String lon, @RequestParam String lat){
+        return lon + lat;
+    }
 
     @RequestMapping("/test")
     String test() {
---
+++ src/main/java/de/ummahuesla/doiwanttolivehere/repository/Noise.java
@@ -0,0 +1,7 @@
+package de.ummahuesla.doiwanttolivehere.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Noise {
+}
