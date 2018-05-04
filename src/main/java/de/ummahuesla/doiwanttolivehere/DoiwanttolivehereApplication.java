1a866b8cf81f1481e1f94156f399a818b2e47219
bohnejoe <bohne-joe@arcor.de>
2018-05-04 16:25:50 +0200

    new project setup

---
+++ src/main/java/de/ummahuesla/doiwanttolivehere/DoiwanttolivehereApplication.java
@@ -0,0 +1,12 @@
+package de.ummahuesla.doiwanttolivehere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoiwanttolivehereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoiwanttolivehereApplication.class, args);
+	}
+}
