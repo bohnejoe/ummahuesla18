553fcd25e89a10eac4f300e090bc7c1c3e340b79
Julian Hirt <julian@couchbits.com>
2018-05-05 13:38:52 +0200

    Add styling for smaller markers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,110 +7,60 @@ class AnyReactComponent extends Component {
   constructor(props) {
     super();
     this.state = {
-      items: []
+      items: [],
+      indicators: []
     }
   }
 
   componentDidUpdate = () => {
-    this._draw();
-    this._tick();
+    // this._generatePositions();
   }
 
-  _tick = () => {
-    this._draw();
-    // this._update();
-    requestAnimationFrame(this._tick);
-  }
+  _generatePositions = () => {
+    var items = [];
 
-  _draw = () => {
-    var canvas = document.getElementById("canvas"),
-        ctx = canvas.getContext('2d');
-
-    this.refs.canvas.width = 400;
-    this.refs.canvas.height = 400;
-    var items = [],
-        FPS = 60, // Frames per second
-        x = 6;
-
-    var rad_distance = (360/x * 3.1415)/180;
-
+    var amount = this.props.indicators ? this.props.indicators.length : 0;
+    var rad_distance = (360/amount * 3.1415)/180;
+    var width = 400;
     // Push items to array
-    for (var i = 0; i < x; i++) {
+    for (var i = 0; i < amount; i++) {
+      var item = this.props.indicators[i];
       var degrees = i*rad_distance;
-      var radius = this.refs.canvas.width*0.3;
-      var xx = this.refs.canvas.width/2.0 + radius*Math.cos(degrees);
-      var xy = this.refs.canvas.height/2.0 + radius*Math.sin(degrees);
+      var radius = width*0.3;
+      var xx = width/2.0 + radius*Math.cos(degrees) - 40;
+      var xy = width/2.0 + radius*Math.sin(degrees) - 40;
       items.push({
         x: xx,
         y: xy,
         radius: 10,
         vx: Math.floor(Math.random() * 50) - 25,
-        vy: Math.floor(Math.random() * 50) - 25
+        vy: Math.floor(Math.random() * 50) - 25,
+        item: item
       });
     }
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.globalCompositeOperation = "lighter";
-
-    for (var i = 0, x = items.length; i < x; i++) {
-      var s = items[i];
-
-      ctx.fillStyle = "#ff0000";
-      ctx.beginPath();
-      ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.fillStyle = 'black';
-      ctx.stroke();
-    }
-
-    ctx.beginPath();
-    for (var i = 0, x = items.length; i < x; i++) {
-      var starI = items[i];
-      ctx.moveTo(starI.x,starI.y);
-
-      ctx.lineTo(this.refs.canvas.height/2.0,this.refs.canvas.width/2.0);
-    }
-    ctx.lineWidth = 0.35;
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-
-    // this.setState({items: items});
-  }
-
-  _update = () => {
-    var FPS = 60;
-
-    for (var i = 0, x = this.state.items.length; i < x; i++) {
-      var s = this.state.items[i];
-
-      s.x += s.vx / FPS;
-      s.y += s.vy / FPS;
-
-      // if (s.x < 0 || s.x > this.refs.canvas.width) s.vx = -s.vx;
-      // if (s.y < 0 || s.y > this.refs.canvas.height) s.vy = -s.vy;
-    }
-  }
-
-  distance = ( point1, point2 ) => {
-    var xs = 0;
-    var ys = 0;
-
-    xs = point2.x - point1.x;
-    xs = xs * xs;
-
-    ys = point2.y - point1.y;
-    ys = ys * ys;
-
-    return Math.sqrt( xs + ys );
+    return items;
   }
 
   render() {
+    var items = this._generatePositions();
+
+    const indicators = items.map((item)=> {
+      const qi = Math.round(item.item.score*10)/10;
+      const markerClassname = 'marker-small marker marker-'+ qi*10;
+      const divStyle = {
+        position:'absolute',
+        left: item.x,
+        top: item.y
+      };
+      return (<div className={markerClassname} style={divStyle}>{qi}<span ref='name'>{item.item.name}</span></div>)
+    });
+
     const data = this.props.text === undefined  ? (
       <div className="lds-ripple"><div></div><div></div></div>
     ) : (<div className={this.props.className}>{this.props.text}</div>)
     return(
       <div className='details'>
-        <canvas ref='canvas' id='canvas'></canvas>
+        {indicators}
         {data}
       </div>
     )
@@ -144,7 +94,13 @@ class SimpleMap extends Component {
     })).then(result => {
       return result.json();
     }).then(data => {
-      this.setState({ lat: data.lat, lng: data.lng, text: Math.round(data.overallScore*10)/10.0, qi: data.overallScore });
+      this.setState({
+        lat: data.lat,
+        lng: data.lng,
+        text: Math.round(data.overallScore*10)/10.0,
+        qi: data.overallScore,
+        indicators: data.inidicators
+      });
     });
   }
 
@@ -161,6 +117,7 @@ class SimpleMap extends Component {
             lat={ this.state.lat }
             lng={ this.state.lng }
             text={ this.state.text }
+            indicators={ this.state.indicators }
             className={ 'marker marker-'+Math.round(this.state.qi*10) }
           />
         </GoogleMapReact>
