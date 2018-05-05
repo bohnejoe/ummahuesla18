f969943b367dfd4192f414d4442cbbc921390a7b
bohnejoe <bohne-joe@arcor.de>
2018-05-05 13:51:22 +0200

    enabled sunlight collector

--- src/main/java/de/ummahuesla/doiwanttolivehere/service/QualityIndexService.java
+++ src/main/java/de/ummahuesla/doiwanttolivehere/service/QualityIndexService.java
@@ -47,7 +47,7 @@ public class QualityIndexService {
     public void init() {
     	collectors.add(nearbyDoctorsCollector);
     	collectors.add(nearbySupermarketsCollector);
-//    	collectors.add(sunHoursPerYearCollector);
+    	collectors.add(sunHoursPerYearCollector);
     	collectors.add(transportConnectionCollector);
     	collectors.add(playgroundCollector);
     	collectors.add(schoolCollector);
